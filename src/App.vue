aba25fc5b38253d6111a3ac16c37c009669c4828
= <=>
2023-03-17 11:48:34 -0300

    projeto 3

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+const contador = ref(10)
+
+function incrementar() {
+  contador.value++
+  console.log(contador)
+}
+</script>
+
+<template>
+  <div>
+    contador: {{ contador }}
+  </div>
+    <button @click="incrementar">+</button>
+    <button @click="$event => contador --">-</button>
+    <button @click="mostrarDiv = !mostrarDiv">Alternar </button>
+
+    <div v-if="mostrarDiv"> Vou mostrar o conteudo... </div>
+</template>
+<style></style>
